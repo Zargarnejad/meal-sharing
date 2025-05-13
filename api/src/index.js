2291c8a82c171fee1f2dc462c14ae987e31bc535
Zargarnejad <108077042+Zargarnejad@users.noreply.github.com>
2025-05-13 20:24:09 +0200

    Implemented initial backend api

--- api/src/index.js
+++ api/src/index.js
@@ -11,14 +11,47 @@ app.use(bodyParser.json());
 
 const apiRouter = express.Router();
 
-// You can delete this route once you add your own routes
-apiRouter.get("/", async (req, res) => {
-  const SHOW_TABLES_QUERY =
-    process.env.DB_CLIENT === "pg"
-      ? "SELECT * FROM pg_catalog.pg_tables;"
-      : "SHOW TABLES;";
-  const tables = await knex.raw(SHOW_TABLES_QUERY);
-  res.json({ tables });
+apiRouter.get("/future-meals", async (req, res) => {
+  const FEUTER_MEAL_QUERY = `
+  select * from meal 
+  where \`when\`> now();
+  `;
+  const meals = await knex.raw(FEUTER_MEAL_QUERY);
+  res.json({ meals });
+});
+
+apiRouter.get("/past-meals", async (req, res) => {
+  const PAST_MEAL_QUERY = `
+  select * from meal 
+  where \`when\`< now();
+  `;
+  const meals = await knex.raw(PAST_MEAL_QUERY);
+  res.json({ meals });
+});
+
+apiRouter.get("/all-meals", async (req, res) => {
+  const ALL_MEAL_QUERY = `
+  select * from meal 
+  order by id`;
+  const meals = await knex.raw(ALL_MEAL_QUERY);
+  res.json({ meals });
+});
+
+apiRouter.get("/first-meal", async (req, res) => {
+  const FIRST_MEAL_QUERY = `
+  select * from meal
+  limit 1`;
+  const data = await knex.raw(FIRST_MEAL_QUERY);
+  checkEmptyResponce(data, res);
+});
+
+apiRouter.get("/last-meal", async (req, res) => {
+  const LAST_MEAL_QUERY = `
+  select * from meal where id = 90
+  order by id desc
+  limit 1`;
+  const data = await knex.raw(LAST_MEAL_QUERY);
+  checkEmptyResponce(data,res)
 });
 
 // This nested router example can also be replaced with your own sub-router
@@ -29,3 +62,14 @@ app.use("/api", apiRouter);
 app.listen(process.env.PORT, () => {
   console.log(`API listening on port ${process.env.PORT}`);
 });
+
+
+function checkEmptyResponce(dbResult, res){
+  const potentialMeal = dbResult[0];
+  if (potentialMeal.length == 0) {
+    res.status(404);
+    res.json({ error: "No meal find in database." });
+  } else {
+    res.json({ dbResult });
+  }
+}
