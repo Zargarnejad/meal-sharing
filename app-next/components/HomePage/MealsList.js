056e57778476e6060fe6c5ca1e31c5a71fd58ea1
Zargarnejad <108077042+Zargarnejad@users.noreply.github.com>
2025-06-28 21:07:05 +0200

    Added error handling when fetching the meals list

--- app-next/components/HomePage/MealsList.js
+++ app-next/components/HomePage/MealsList.js
@@ -5,23 +5,43 @@ import "./HomePage.css";
 
 function MealsList() {
   const [meals, setMeals] = useState([]);
+  const [dataLoadState, setDataLoadState] = useState("LOADING");
 
   useEffect(() => {
     const fetchMeals = async () => {
-      const mealsResponse = await fetch(
-        "http://localhost:3001/api/meals/"
-      ).then((response) => response.json());
+      const mealsResponse = await fetch("http://localhost:3001/api/meals/")
+        .then((response) => response.json())
+        .catch((e) => {
+          setDataLoadState("LOADING_FAILED");
+        });
 
-      setMeals(mealsResponse.results);
+      if (mealsResponse !== undefined) {
+        setDataLoadState("LOADING_SUCCEEDED");
+        setMeals(mealsResponse.results);
+      }
     };
 
     fetchMeals();
   }, []);
 
+  // Prepare a message to show in the page in case of error or loading
+  let message = <></>;
+  switch (dataLoadState) {
+    case "LOADING_FAILED":
+      message = (
+        <span className="errorMessage">Loading meals list failed ;(</span>
+      );
+      break;
+    case "LOADING":
+      message = "Loading list of meals...";
+      break;
+  }
+
   return (
     <div className="mealsListContainer">
       <h1>Meals List:</h1>
       <ul className="mealsList">
+        {message}
         {meals.map((meal, index) => {
           return (
             <li key={index} className="mealsListItem">
