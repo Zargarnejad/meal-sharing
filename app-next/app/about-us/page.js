09fbc2e03f4c91cdc27264a7bec2c5a29ddf6d3d
Zargarnejad <108077042+Zargarnejad@users.noreply.github.com>
2025-07-10 16:05:26 +0200

    Improve about us layout and styles

--- app-next/app/about-us/page.js
+++ app-next/app/about-us/page.js
@@ -8,56 +8,56 @@ export default function AboutPage() {
           <div className={styles.aboutUsLeftSide}></div>
           <div className={styles.aboutUsRightSide}>
             <h2>About Us</h2>
-            <p>
-              Welcome to MealSharing – where good food brings people together.
-              <br />
-              We believe meals are more than just food — they’re stories,
-              memories, and connections. That’s why we created this platform: to
-              connect passionate home cooks and food lovers around the world.
-              <br />
-            </p>
-            <p>
-              <b>🍽 Our Mission</b>
-              <br />
-              To make dining more personal by allowing people to:
+            <div>
+              <p>
+                Welcome to MealSharing – where good food brings people together.
+              </p>
+              <p>
+                We believe meals are more than just food — they’re stories,
+                memories, and connections. That’s why we created this platform:
+                to connect passionate home cooks and food lovers around the
+                world.
+              </p>
+            </div>
+            <div>
+              <h4>🍽 Our Mission</h4>
+              <p>To make dining more personal by allowing people to:</p>
               <ul>
                 <li>Share home-cooked meals</li>
                 <li>Discover new cultures through food</li>
                 <li>Meet amazing people around the table</li>
               </ul>
-            </p>
-            <p>
-              <b>🌍 What We Offer</b>
-              <br />
-
+            </div>
+            <div>
+              <h4>🌍 What We Offer</h4>
               <ul>
                 <li>
-                  Host a meal: Cook your favorite dish and open your table to
-                  guests.
+                  <b>Host a meal</b>: Cook your favorite dish and open your
+                  table to guests.
                 </li>
                 <li>
-                  Join a meal: Explore home-hosted meals in your city or while
-                  traveling.
+                  <b>Join a meal</b>: Explore home-hosted meals in your city or
+                  while traveling.
                 </li>
                 <li>
-                  Share your experience: Leave reviews and connect with the
-                  community.
+                  <b>Share your experience</b>: Leave reviews and connect with
+                  the community.
                 </li>
               </ul>
-            </p>
-            <p>
-              <b>🤝 Why We Do It</b>
-              <br />
-              In a world moving fast, we slow down — one meal at a time.
-              <br />
-              We believe in real conversations, local experiences, and that
-              every shared bite is a chance to build something meaningful.
-            </p>
-            <p>
+            </div>
+            <div>
+              <h4>🤝 Why We Do It</h4>
+              <p>In a world moving fast, we slow down — one meal at a time.</p>
+              <p>
+                We believe in real conversations, local experiences, and that
+                every shared bite is a chance to build something meaningful.
+              </p>
+            </div>
+            <div>
               Whether you're a passionate cook or a curious guest — there's a
               seat at the table for you. Let's eat, connect, and share something
               unforgettable.
-            </p>
+            </div>
           </div>
         </div>
       </div>
